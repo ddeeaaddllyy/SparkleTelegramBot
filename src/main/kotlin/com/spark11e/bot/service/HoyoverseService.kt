1dee3c9e7bb44303ab25aced5c0c7385b056a4ac
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-22 18:08:30 +0300

    add new logics

--- src/main/kotlin/com/spark11e/bot/service/HoyoverseService.kt
+++ src/main/kotlin/com/spark11e/bot/service/HoyoverseService.kt
@@ -1,55 +1,83 @@
 package com.spark11e.bot.service
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.spark11e.bot.hoyoverse.ApiResponse
-import com.spark11e.bot.hoyoverse.HoyolabProperty
-import com.spark11e.bot.hoyoverse.HsrUserStats
+import com.spark11e.bot.hoyoverse.HoyoverseProperty
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
 import okio.IOException
 import org.springframework.stereotype.Component
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+@OptIn(ExperimentalCoroutinesApi::class)
+public suspend fun Call.await(): Response {
+
+    return suspendCancellableCoroutine { continuation ->
+        enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                if (continuation.isCancelled) return
+
+                continuation.resumeWithException(e)
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                continuation.resume(response)
+            }
+        })
+
+
+    }
+}
 
 @Component
-open class HoyoverseService {
-    companion object{
-        private val client = OkHttpClient()
-        private val mapper = ObjectMapper()
-    }
-    lateinit var hoyolabProperty: HoyolabProperty
-    private val cookie: String = hoyolabProperty.hoyolabCookie
-    private val honkaiStarRailUrl: String = hoyolabProperty.honkaiStarRailUrl
+open class HoyoverseService(
+    private val hoyoverseProperty: HoyoverseProperty,
+    private val client: OkHttpClient
+) {
+    private val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
 
+    private final val honkaiStarRailUrl: String = hoyoverseProperty.honkaiStarRailUrl
 
-    fun getHSRUserData(uid: String): HsrUserStats? {
-        val requestUrl = honkaiStarRailUrl + uid
+    private val responseAdapter = moshi.adapter(ApiResponse::class.java)
 
+    suspend fun fetchHsrStats(uid: String): Result<ApiResponse> {
         val request = Request.Builder()
-            .url(requestUrl)
-            .header("Cookie", cookie)
+            .url("${honkaiStarRailUrl}${uid}/?info")
             .build()
 
         return try {
-            client.newCall(request).execute().use { response ->
-                if (!response.isSuccessful) throw IOException("Неожиданный код ответа: $response")
+            client.newCall(request).await().use { response ->
 
-                val responseBody = response.body?.string() ?: return null
-
-                val apiResponse = mapper.readValue(
-                    responseBody,
-                    mapper.typeFactory.constructParametricType(ApiResponse::class.java, HsrUserStats::class.java)
-                ) as ApiResponse<HsrUserStats>
-
-                if (apiResponse.retcode == 0) {
-                    apiResponse.data
-                } else {
-                    println("API вернул ошибку (${apiResponse.retcode}): ${apiResponse.message}")
-                    null
+                if (!response.isSuccessful) {
+                    val error = response.body?.string()
+                    return Result.failure(IOException("HTTP Error: ${response.code}/${error}"))
                 }
+                val jsonBody = response.body?.string()
+                val result = jsonBody?.let { responseAdapter.fromJson(it) }
+
+                if (result == null) {
+                    Result.failure(Exception("body is empty"))
+                }
+                else {
+                    Result.success(result)
+                }
+
             }
         } catch (e: Exception) {
-            println("Ошибка HTTP-запроса или парсинга: ${e.message}")
-            e.printStackTrace()
-            null
+            Result.failure(e)
         }
     }
+
+
 }
