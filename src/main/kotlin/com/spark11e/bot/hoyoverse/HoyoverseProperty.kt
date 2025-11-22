a295d1a5e1054bac14d60424e19ea5e2fdf62853
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-22 18:07:37 +0300

    create new url properties

---
+++ src/main/kotlin/com/spark11e/bot/hoyoverse/HoyoverseProperty.kt
@@ -0,0 +1,12 @@
+package com.spark11e.bot.hoyoverse
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConfigurationProperties(prefix = "hoyoverse")
+data class HoyoverseProperty(
+    val honkaiStarRailUrl: String = "https://enka.network/api/hsr/uid/",
+    val genshinImpactUrl: String = "https://enka.network/api/uid/",
+    val zenlessZoneZeroUrl: String = "https://enka.network/api/zzz/uid/"
+)
