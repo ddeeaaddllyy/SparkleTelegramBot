be6de4701a2db69ae9c7c97592c3137df1b1dd11
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-22 18:04:34 +0300

    delete useless jsonProperty

--- src/main/kotlin/com/spark11e/bot/hoyoverse/ApiResponse.kt
+++ src/main/kotlin/com/spark11e/bot/hoyoverse/ApiResponse.kt
@@ -1,9 +1,15 @@
 package com.spark11e.bot.hoyoverse
 
-import com.fasterxml.jackson.annotation.JsonProperty
+import com.squareup.moshi.JsonClass
 
-data class ApiResponse<T>(
-    @JsonProperty("retcode") val retcode: Int,
-    @JsonProperty("message") val message: String,
-    @JsonProperty("data") val data: T?
+@JsonClass(generateAdapter = true)
+data class ApiResponse(
+    val detailInfo: DetailInfo,
+    val ttl: Int,
+    val uid: String
+)
+
+@JsonClass(generateAdapter = true)
+data class RecordInfo(
+    val achievementCount: Int
 )
