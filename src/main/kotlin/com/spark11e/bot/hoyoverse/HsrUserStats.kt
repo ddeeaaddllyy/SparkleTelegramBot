825a07aa3007af69fbb1c0561bab3075b69adb6c
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-22 18:08:47 +0300

    delete useless class

--- src/main/kotlin/com/spark11e/bot/hoyoverse/HsrUserStats.kt
+++
@@ -1,9 +0,0 @@
-package com.spark11e.bot.hoyoverse
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class HsrUserStats(
-    @JsonProperty("UID") val uid: String,
-    @JsonProperty("level") val level: Int,
-    @JsonProperty("world_level") val equilibriumLevel: Int
-)
