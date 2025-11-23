065e8425b2e19b561707cf88a5500eb939b60d4a
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-23 21:56:12 +0300

    create something (idk what)

--- src/main/kotlin/com/spark11e/bot/hoyoverse/HoyoverseProperty.kt
+++ src/main/kotlin/com/spark11e/bot/hoyoverse/HoyoverseProperty.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationProperties(prefix = "hoyoverse")
-data class HoyoverseProperty(
+public data class HoyoverseProperty(
     val honkaiStarRailUrl: String = "https://enka.network/api/hsr/uid/",
     val genshinImpactUrl: String = "https://enka.network/api/uid/",
     val zenlessZoneZeroUrl: String = "https://enka.network/api/zzz/uid/"
