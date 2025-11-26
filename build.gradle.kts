8800861537736c48ff19ce722e1c07fcad0e7328
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-26 18:48:28 +0300

    add 4 new implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     ksp("com.squareup.moshi:moshi-kotlin-codegen:${moshiVersion}")
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.hibernate:hibernate-community-dialects:6.5.0.Final")
 	implementation("org.xerial:sqlite-jdbc:3.45.1.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
