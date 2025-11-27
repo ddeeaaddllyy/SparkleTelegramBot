1eccf5b8bd72a85d32195c911e337e6420f0fff8
ddeeaaddllyy <naumova070783@gmail.com>
2025-11-27 17:34:58 +0300

    Add new bot command

--- src/main/kotlin/com/spark11e/bot/telegram/BotCommands.kt
+++ src/main/kotlin/com/spark11e/bot/telegram/BotCommands.kt
@@ -13,7 +13,7 @@ enum class BotCommands(
 {
     START(command = "/start", description = "bot start"),
     HELP(command = "/help", description = "show all commands"),
-    INFO(command = "/info", description = "about bot"),
     HSR_STATS(command = "/hoyostats", description = "show you hsr account by UID"),
-    USER_ACCOUNT(command = "/account", description = "in process" )
+    USER_ACCOUNT(command = "/account", description = "show your account" ),
+    SET_PHOTO(command = "/setphoto", description = "here u can set the photo of your profile" )
 }
